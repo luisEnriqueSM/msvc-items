86747dc1fc46a6830c0464a58f965f94cfa68946
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2025-03-19 22:59:47 -0600

    Implementing create, update and delete methos from ItemServiceWebClient on ItemController.

--- src/main/java/com/luis/springcloud/msvc/items/controllers/ItemController.java
+++ src/main/java/com/luis/springcloud/msvc/items/controllers/ItemController.java
@@ -16,9 +16,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -145,5 +150,21 @@ public class ItemController {
             product.setPrice(500D);
             return ResponseEntity.ok(new Item(product, 5));
         });
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> create(@RequestBody Product product){
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.itemService.save(product));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@RequestBody Product product, @PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.itemService.update(product, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        this.itemService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
